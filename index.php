507ed00ad9525474e96d85bf9269fd7484a811ff
Bali Tenaya <balitenaya666@gmail.com>
2021-09-01 22:00:55 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <style>
+        @font-face {
+            font-family: "Font Digital";
+            src: url('font/Karla-Bold.ttf');
+        }
+
+        html {
+            min-height: 100%;
+        }
+
+        .atas1 {
+            margin-top: 2%;
+            font-family: "Font Digital";
+            bottom: 0px;
+        }
+
+        .bg {
+            background: radial-gradient(circle at top left, #505152, #666869);
+            position: static;
+        }
+
+        .button1 {
+            margin-top: 2%;
+        }
+
+        .tulisan {
+            margin-top: 1%;
+            /* font-family: Arial, Helvetica, sans-serif; */
+            color: whitesmoke;
+        }
+
+        #kaki {
+            font-family: monospace;
+            color: whitesmoke;
+            opacity: 0.2;
+            align-items: center;
+            z-index: 1;
+            position: fixed;
+            left: 0px;
+            margin-left: 5%;
+            width: 100%;
+            bottom: 0px;
+
+        }
+    </style>
+    <link rel="icon" type="image/png" href="logo/en.png">
+    <title>I DEWA GEDE BALI TENAYA</title>
+
+
+</head>
+
+<body class="bg">
+    <center>
+        <div class="container atas1">
+            <img src="logo/bali.jpg" height="96" width="96" class="rounded-circle">
+            <h5 class="tulisan">@tenaya_bali</h5>
+            <div class="button1">
+                <a type="button" href="https://instagram.com/tenaya_bali/" class="btn btn-outline-light btn-lg btn-block">Instagram</a>
+                <a type="button" href="https://twitter.com/tenaya_bali" class="btn btn-outline-light btn-lg btn-block">Twitter</a>
+                <a type="button" href="https://facebook.com/balistream01" class="btn btn-outline-light btn-lg btn-block">Facebook Gamming</a>
+                <a type="button" href="https://www.youtube.com/balitenayaa" class="btn btn-outline-light btn-lg btn-block">Youtube</a>
+                <a type="button" href="https://discord.gg/9S928uzdTd " class="btn btn-outline-light btn-lg btn-block">Discord</a>
+                <a type="button" href="https://www.tiktok.com/@tenaya_bali" class="btn btn-outline-light btn-lg btn-block">TikTok</a>
+            </div>
+        </div>
+    </center>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
